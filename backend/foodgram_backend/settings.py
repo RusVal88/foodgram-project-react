712581edfd385741dfe79414115ba1840bcad4cc
RusVal88 <ruslan.suhanoff@yandex.com.com>
2023-07-25 22:57:07 +0500

    edits6

--- backend/foodgram_backend/settings.py
+++ backend/foodgram_backend/settings.py
@@ -6,12 +6,11 @@ from dotenv import load_dotenv
 
 
 BASE_DIR = Path(__file__).resolve().parent.parent
-"""
+
 env_path = os.path.join(BASE_DIR.parent, 'infra/.env')
 
 load_dotenv(env_path)
-"""
-load_dotenv()
+
 
 SECRET_KEY = os.getenv('SECRET_KEY', 'default_key')
 
